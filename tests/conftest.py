9e6179070776bff0bf7bb4ecc8b47a69295300be
Alex <alex.d.bondarev@gmail.com>
2021-07-17 11:50:44 +0100

    run isort

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
 
-from tests.fixtures.app_client_fixtures import app_context, test_client, db_fixture
+from tests.fixtures.app_client_fixtures import app_context, db_fixture, test_client
 from tests.fixtures.test_flow_fixtures import (
-    test_account_data,
-    test_account,
     cleanup_test_account,
+    test_account,
+    test_account_data,
 )
 
 # Credit for modularization to https://gist.github.com/peterhurford/09f7dcda0ab04b95c026c60fa49c2a68

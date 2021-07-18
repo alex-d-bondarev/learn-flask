f0e22b0ba61d2708189499de44a214b188e4d8b5
Alex <alex.d.bondarev@gmail.com>
2021-07-18 11:40:26 +0100

    add delay PUT stub

--- learn_app/test_flow/endpoints/delay.py
+++ learn_app/test_flow/endpoints/delay.py
@@ -1,17 +1,20 @@
-from flask import json
+from flask import json, request
 
 from learn_app.main import app
 
 
-@app.route("/delay")
+@app.route("/delay", methods=["GET", "PUT"])
 def get_delay():
     """Get processing delay
 
     :return:
     """
-    default_delay = {
-        "max_delay": 3000,
-        "random": True,
-    }
-    json_body = json.dumps(default_delay)
-    return app.response_class(response=json_body, status=200)
+    if request.method == "GET":
+        default_delay = {
+            "max_delay": 3000,
+            "random": True,
+        }
+        json_body = json.dumps(default_delay)
+        return app.response_class(response=json_body, status=200)
+    else:
+        return app.response_class(status=204)
--- tests/test_flow/delay_integration_test.py
+++ tests/test_flow/delay_integration_test.py
@@ -32,3 +32,21 @@ def test_empty_delay_shows_random_3000(test_client, db_fixture):
 
     assert json_response["max_delay"] == 3000
     assert json_response["random"]
+
+
+@pytest.mark.usefixtures("test_client")
+def test_delay_put_command_is_204(test_client):
+    response = test_client.put("/delay")
+    assert response.status_code == 204
+
+
+# @pytest.mark.usefixtures("test_client")
+# def test_delay_update_max_delay(test_client):
+#     new_max_delay = 1000
+#     update_data = {"max_delay": new_max_delay}
+#     response = test_client.put("/delay", data=update_data)
+#     response = test_client.get("/delay")
+#     json_response = json.loads(response.data)
+#
+#     assert json_response["max_delay"] == new_max_delay
+#     assert json_response["random"]
